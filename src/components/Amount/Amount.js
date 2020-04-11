8aca5de06b385e3e8215b3ebf1e1fe3823ab5548
loryma <lorymalloc@gmail.com>
2020-04-11 11:37:10 +0300

    add validation for amount

--- src/components/Amount/Amount.js
+++ src/components/Amount/Amount.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 import * as actions from "modules";
 import Input from "../Input";
@@ -6,21 +6,32 @@ import Currency from "components/Currency";
 import s from "./Amount.module.scss";
 
 function Amount({ setAmount, setCurrency, amount, currency }) {
+  const [amountTouched, setAmountTouched] = useState(false);
+  const amountIsInvalid = amountTouched && !amount.trim();
+
   const handleAmountChange = (e) => {
     const newValue = e.target.value;
-    setAmount(newValue);
+    const formattedValue = newValue.replace(/\D+/, "");
+    setAmount(formattedValue);
+    if (!amountTouched) setAmountTouched(true);
   };
 
   const handleCurrencyChange = ({ value }) => {
     setCurrency(value);
   };
 
+  const onBlur = (e) => setAmountTouched(true);
+
   return (
     <div className={s.field}>
-      <label className={s.label}>Amount</label>
+      <label className={`${s.label} ${amountIsInvalid ? s.inputInvalid : ""}`}>
+        Amount
+      </label>
       <Input
         placeholder="100"
+        type="number"
         className={s.input}
+        onBlur={onBlur}
         onChange={handleAmountChange}
         value={amount}
       />
