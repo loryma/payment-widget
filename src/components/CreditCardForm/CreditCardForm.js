9e712fc0a91c3546f5bdc3f31d1f6a6ae692cb51
loryma <lorymalloc@gmail.com>
2020-04-11 03:40:33 +0300

    add styles for error

--- src/components/CreditCardForm/CreditCardForm.js
+++ src/components/CreditCardForm/CreditCardForm.js
@@ -14,7 +14,7 @@ import Modal from "shared/Modal";
 import s from "./CreditCardForm.module.scss";
 
 function CreditCardForm({ amount, currency, submitForm }) {
-  const submitText = `Pay ${amount} ${currency}`;
+  const submitText = amount ? `Pay ${amount} ${currency}` : "Pay";
   const [isOpen, setIsOpen] = useState(false);
 
   const formik = useFormik({
@@ -26,22 +26,22 @@ function CreditCardForm({ amount, currency, submitForm }) {
     },
     validationSchema: object({
       cardholderName: string()
-        .max(200, "Must be 200 characters or less")
-        .min(2, "Must be at 2 characters or more")
+        .max(200, "Card holder name must be 200 characters or less")
+        .min(2, "Card holder name must be at 2 characters or more")
         .matches(/^([A-Za-z]|-|\s)+$/, {
           message: "Name can contain only characters",
         })
-        .required("Requered"),
+        .required("Card holder name is required"),
       cardNumber: string()
+        .max(19, "Card number must be 16 numbers or less")
+        .min(16, "Card number must be 13 numbers or more")
         .test("Luhn-algo", "Card number is invalid", function (value) {
           if (value) {
             return checkLuhn(unmaskCardNumber(value));
           }
           return false;
         })
-        .max(19, "Must be 16 numbers or less")
-        .min(16, "Must be 13 numbers or more")
-        .required("Requered"),
+        .required("Card number is required"),
       expDate: string()
         .test("date-in-future", "Expiration date is invalid", function (value) {
           if (value) {
@@ -49,12 +49,12 @@ function CreditCardForm({ amount, currency, submitForm }) {
           }
           return false;
         })
-        .required("Requered"),
+        .required("Expiration date is required"),
       cvv: string()
-        .max(4, "Must be 3 or 4 numbers")
-        .min(3, "Must be 3 or 4 numbers")
+        .max(4, "CVC must be 3 or 4 numbers")
+        .min(3, "CVC must be 3 or 4 numbers")
         .matches(/^\d+$/, { message: "CVC can contain only numbers" })
-        .required("Requered"),
+        .required("CVC is required"),
     }),
     onSubmit: (values) => {
       const formValues = { ...values, amount, currency };
@@ -62,6 +62,36 @@ function CreditCardForm({ amount, currency, submitForm }) {
       submitForm(formValues).then(() => setIsOpen(true));
     },
   });
+
+  const cardNameError =
+    formik.touched.cardholderName && formik.errors.cardholderName
+      ? formik.errors.cardholderName
+      : null;
+
+  const cardNumberError =
+    formik.touched.cardNumber && formik.errors.cardNumber ? (
+      <div>{formik.errors.cardNumber}</div>
+    ) : null;
+
+  const expDateError =
+    formik.touched.expDate && formik.errors.expDate ? (
+      <div>{formik.errors.expDate}</div>
+    ) : null;
+
+  const cvvError =
+    formik.touched.cvv && formik.errors.cvv ? (
+      <div>{formik.errors.cvv}</div>
+    ) : null;
+
+  const formError =
+    cardNameError ||
+    cardNameError ||
+    cardNumberError ||
+    expDateError ||
+    cvvError;
+
+  const submitDisabled = !currency || !amount || formError;
+
   return (
     <>
       <Modal
@@ -74,7 +104,7 @@ function CreditCardForm({ amount, currency, submitForm }) {
         </span>
         <p className={s.modalMessage}>Card details successfully submitted</p>
       </Modal>
-      <form onSubmit={formik.handleSubmit}>
+      <form className={s.form} onSubmit={formik.handleSubmit}>
         <div className={s.field}>
           <label className={s.label} htmlFor="cardholderName">
             Name
@@ -89,60 +119,50 @@ function CreditCardForm({ amount, currency, submitForm }) {
             onBlur={formik.handleBlur}
             value={formik.values.cardholderName}
           />
-          {formik.touched.cardholderName && formik.errors.cardholderName ? (
-            <div className={s.error}>{formik.errors.cardholderName}</div>
-          ) : null}
         </div>
-        <div>
-          <div>
-            <MaskedInput
-              mask={CARD_MASK}
-              guide={false}
-              id="cardNumber"
-              name="cardNumber"
-              type="text"
-              placeholder="Card number"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.cardNumber}
-            />
-            {formik.touched.cardNumber && formik.errors.cardNumber ? (
-              <div>{formik.errors.cardNumber}</div>
-            ) : null}
-          </div>
-          <div>
-            <MaskedInput
-              mask={EXPIRATION_MASK}
-              guide={false}
-              id="expDate"
-              name="expDate"
-              type="text"
-              placeholder="MM / YY"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.expDate}
-            />
-            {formik.touched.expDate && formik.errors.expDate ? (
-              <div>{formik.errors.expDate}</div>
-            ) : null}
-          </div>
-          <div>
-            <input
-              id="cvv"
-              name="cvv"
-              type="text"
-              placeholder="CVC"
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              value={formik.values.cvv}
-            />
-            {formik.touched.cvv && formik.errors.cvv ? (
-              <div>{formik.errors.cvv}</div>
-            ) : null}
-          </div>
+        <div className={s.field}>
+          <MaskedInput
+            mask={CARD_MASK}
+            guide={false}
+            id="cardNumber"
+            name="cardNumber"
+            className={`${s.input} ${s.cardNumber}`}
+            type="text"
+            placeholder="Card number"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.cardNumber}
+          />
+          <MaskedInput
+            mask={EXPIRATION_MASK}
+            guide={false}
+            id="expDate"
+            name="expDate"
+            className={`${s.input} ${s.expDate}`}
+            type="text"
+            placeholder="MM / YY"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.expDate}
+          />
+          <input
+            id="cvv"
+            name="cvv"
+            className={`${s.input} ${s.cvv}`}
+            type="text"
+            placeholder="CVC"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.cvv}
+          />
         </div>
 
-        <button type="submit">{submitText}</button>
+        <button className={s.button} type="submit">
+          {submitText}
+        </button>
+        <div className={`${s.error} ${formError ? s.errorVisible : ""}`}>
+          {formError}
+        </div>
       </form>
     </>
   );
